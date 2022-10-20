ecd4c0f41ab37993483620ef4d9a932cdcfec75a
Thomas <thomas.lawanson@gmail.com>
2022-10-20 13:38:59 +0100

    Launcher and instructuins

---
+++ launcher.sh
@@ -0,0 +1,9 @@
+# Sleep for 90 seconds. This number is set to 90 to give time to stop the script running automatically
+#sleep 90
+
+
+omxplayer FILENAME.mp4 #replace `FILENAME.mp4` with your own video
+
+# replace `FILENAME.mp4` with your own video
+# the options for --aspect-mode are Letterbox, fill and stretch. 
+#omxplayer FILENAME.mp4 --loop --no-osd --aspect-mode=letterbox
