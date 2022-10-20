635eccebfa75f69462c0fbbadb9dc4dbab4a857c
Thomas <thomas.lawanson@gmail.com>
2022-10-20 18:11:32 +0100

    Update

--- launcher.sh
+++ launcher.sh
@@ -1,6 +1,7 @@
 # Sleep for 90 seconds. This number is set to 90 to give time to stop the script running automatically
 #sleep 90
 
+cd /home/pi/Desktop/PiVideo-master
 
 omxplayer FILENAME.mp4 #replace `FILENAME.mp4` with your own video
 
